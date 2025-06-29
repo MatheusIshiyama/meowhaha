4df123329e31468fc3b5c11778200fc59662bcd8
Matheus Ishiyama <matheus.ishiyama@outlook.com>
2025-06-29 11:39:27 +0900

    🔧 refactor: add comments to logger function for improved code clarity

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -16,8 +16,9 @@ export function logger(type: LoggerType, title: string, message: string | unknow
     ERROR: '❌',
   };
 
-  console.log(logColors[type]);
-  console.log(`[${title}] ${logEmoji[type]} ${message}`);
-  console.log('\x1b[0m');
+  console.log(logColors[type]); // ? Define the color of the log
+  console.log(`[${title}] ${logEmoji[type]} ${message}`); // ? Log the message
+  console.log('\x1b[0m'); // ? Reset the color
+
   if (['ERROR', 'WARNING'].includes(type)) console.log(message);
 }
